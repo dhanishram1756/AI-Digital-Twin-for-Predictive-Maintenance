a19fba67eadae188c41317e0761e87820c324d97
dhanishram1756 <dhanishram.1756@gmail.com>
2026-09-02 03:38:09 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -500,4 +500,4 @@ if __name__ == '__main__':
     print("="*70)
     print("\nPress Ctrl+C to stop the server\n")
     
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
